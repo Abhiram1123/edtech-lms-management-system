fd689d7ed996ae8b701bcf3c6a1998a16dbbca28
GurralaSindhuja <gurralasindhuja15@gmail.com>
2026-05-30 20:44:03 +0530

    Add PostgreSQL database config

--- backend/settings.py
+++ backend/settings.py
@@ -59,8 +59,12 @@ TEMPLATES = [
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'Rajitha@123',
+        'HOST': 'db.vgbjznzsmyqfcphxhgkk.supabase.co',
+        'PORT': '5432',
     }
 }
 
